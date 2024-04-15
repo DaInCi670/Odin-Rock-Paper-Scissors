f60bc5cb4d0483339aa03ddd4dc03a2e9e96095b
Emmanuel Owusu Yeboah <yeboahemmanuel913@gmail.com>
2024-04-15 00:33:51 +0000

    Add function to play a round of the game

--- script.js
+++ script.js
@@ -13,7 +13,33 @@ function getComputerChoice() {
 
 //Function to get player choice
 function getplayerSelection() {
-  let playerChoice = prompt("Rock, Paper or Scissors:");
+  let playerChoice = prompt("Rock, Paper or Scissors:").toLowerCase();
   return playerChoice;
 }
 
+//Function to play a round
+function playRound(a, b) {
+  if (a === "rock") {
+    if (b === "paper") {
+      return "You lose! Paper beats Rock";
+    } else if (b === "scissors") {
+      return "You win! Rock beats Scissors";
+    } else return "Its a tie!";
+  } else if (a === "paper") {
+    if (b === "scissors") {
+      return "You lose! Scissors beats Paper";
+    } else if (b === "rock") {
+      return "You win! Paper beats Rock";
+    } else return "Its a tie!";
+  } else if (a === "scissors") {
+    if (b === "rock") {
+      return "You lose! Rock beats Scissors";
+    } else if (b === "paper") {
+      return "You win! Scissors beats Paper";
+    } else return "Its a tie!";
+  } else {
+    return "fuck";
+  }
+}
+
+console.log(playRound(getplayerSelection(), getComputerChoice()));
